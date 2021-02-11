a3535e43de1da7a1fc99aa93d94dcd0f2a5edfde
Cristina Tato <c.tatodelatorre@gmail.com>
2021-02-11 20:33:19 +0100

    fetch and view the gifs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const GIFS =  ['https://media1.giphy.com/media/2AilMg2L8rTAA/200w.webp?cid=ecf05e47nt6w43cw010mnyshen2rx6pfe6gv02dxno0umi3u&rid=200w.webp','https://media3.giphy.com/media/kLLDVeWnNwRXO/giphy.webp?cid=ecf05e47nt6w43cw010mnyshen2rx6pfe6gv02dxno0umi3u&rid=giphy.webp']
-const DIFFERENT_GIFS = ['https://media1.giphy.com/media/1xK1QRqHENTTW/200w.webp?cid=ecf05e47nt6w43cw010mnyshen2rx6pfe6gv02dxno0umi3u&rid=200w.webp']
+const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=Okpaxk39zhRA4tS8eX0hYeDtmna09GwW&q=leslie knope&limit=25&offset=0&rating=g&lang=en'
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      const {data} = response
+      const gifs = data.map(image => image.images.downsized_medium.url)
+      setGifs(gifs)
+    })
+  }, [])
+
   return (
     <div className="App">
       <section className="App-content">
         {
-          gifs.map(singleGif => <img src={singleGif}/>)
+          gifs.map((singleGif, index) => {
+          return <img src={singleGif} alt="imagen"/>
+        })
         }
-        <button onClick={()=>setGifs(DIFFERENT_GIFS) }>Cambiar gifs</button>
+      
       </section>
     </div>
   );
